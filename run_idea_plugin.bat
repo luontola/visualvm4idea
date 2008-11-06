16e12e76840e0ee557bb39f78a124c5bfaaab639
Esko Luontola <esko.luontola@gmail.com>
2008-11-06 19:06:22 +0000

    "mvn clean" before running plugin

--- run_idea_plugin.bat
+++ run_idea_plugin.bat
@@ -1,5 +1,5 @@
 @echo off
-call mvn package
+call mvn clean package
 call rmdir /s /q "%USERPROFILE%\.IntelliJIdea70\system\plugins-sandbox\plugins\visualvm4idea"
 call "C:\Program Files\7-Zip\7z.exe" x -y -o"%USERPROFILE%\.IntelliJIdea70\system\plugins-sandbox\plugins" visualvm4idea-dist\target\visualvm4idea-*.zip
 call "C:\Program Files\JetBrains\idea-7.0.4-jdk15\bin\idea.exe"
